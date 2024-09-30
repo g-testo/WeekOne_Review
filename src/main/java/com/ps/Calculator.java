a7237cdce6f4d3866721373ef7a9f88d95f77af7
Greg <gt@gregory.com>
2024-09-30 12:09:20 -0400

    created addition method

--- src/main/java/com/ps/Calculator.java
+++ src/main/java/com/ps/Calculator.java
@@ -2,10 +2,15 @@ package com.ps;
 
 class Calculator {
     public static void main(String[] args){
-
+        int sum = addition(200, 500);
+        System.out.println(sum);
     }
 
     // Static add method
+    public static int addition(int numberOne, int numberTwo){
+        return numberOne + numberTwo;
+    }
+
     // Static subtract method
     // Static multiplication method
     // Static division method
