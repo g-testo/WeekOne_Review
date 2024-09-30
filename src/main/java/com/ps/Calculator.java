1675e1e56c47bcf5f7f45f709830654b324e021e
Greg <gt@gregory.com>
2024-09-30 11:50:12 -0400

    first commit

---
+++ src/main/java/com/ps/Calculator.java
@@ -0,0 +1,23 @@
+package com.ps;
+
+class Calculator {
+    public static void main(String[] args){
+
+    }
+
+    // Static add method
+    // Static subtract method
+    // Static multiplication method
+    // Static division method
+
+    // Receive two inputs (ints)
+    // Return a single number (int, double)
+
+
+    // Extras
+    // Static modulo method
+    // Three inputs
+
+    // Calculate area
+    // Calculate perimeter
+}
